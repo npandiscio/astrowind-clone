e15aff72522df8e8e74b2f8dd4422746a7147ef2
npandiscio <nick@engineeredinbound.com>
2026-03-19 11:31:45 -0500

    Create AstroWind default catalog and deploy to GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://npandiscio.github.io',
+  base: '/astrowind-clone',
   output: 'static',
 
   integrations: [
